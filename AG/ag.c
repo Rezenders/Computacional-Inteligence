483e455980d8f9df516a348216c46153c1a18e05
Gustavo Rezende Silva <gustavorezendesilva@hotmail.com>
2017-04-16 20:16:13 -0300

    Funcões criadas para otimizar e tornar o código mais legível. Selecionando 80 pais

--- AG/ag.c
+++ AG/ag.c
@@ -3,46 +3,69 @@
 #include "time.h"
 #include <string.h>
 
+void sort(int array[]);
+int getAv(int array[]);
+void getMinMaxAv();
+void selectParents(int p_index[]);
+
 int main() {
   // sendmory
-  int p[100][10];
+  int p[100][11];
+  int p_index[80];
+
   int n[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
   int av_min = 1000000000, av_max = 0;
   int av_mini = 0, av_maxi = 0;
 
-  time_t current_time;
   srand((unsigned)666);
-  // srand((unsigned) time(&current_time));
 
   for (int j = 0; j < 100; j++) {
-    for (int i = 9; i > 0; --i) {
-      // index
-      int w = rand() % i;
-      // swap
-      int t = n[i];
-      n[i] = n[w];
-      n[w] = t;
-    }
-
+    sort(n);
     memcpy(p[j], n, sizeof(int) * 10);
 
-    p[j][11] =
-        abs((p[j][0] * 1000 + p[j][1] * 100 + p[j][2] * 10 + p[j][3] * 1) +
-            (p[j][4] * 1000 + p[j][5] * 100 + p[j][6] * 10 + p[j][1] * 1) -
-            (p[j][4] * 10000 + p[j][5] * 1000 + p[j][2] * 100 + p[j][1] * 10 +
-             p[j][7] * 1));
+    p[j][10] = getAv(p[j]);
 
-    if (p[j][11] < av_min) {
-      av_min = p[j][11];
+    if (p[j][10] < av_min) {
+      av_min = p[j][10];
       av_mini = j;
     }
 
-    if (p[j][11] > av_max) {
-      av_max = p[j][11];
+    if (p[j][10] > av_max) {
+      av_max = p[j][10];
       av_maxi = j;
     }
   }
   // printf("size of int %i \n", sizeof(int));
   printf("Av min: %i  Av min index: %i\n", av_min, av_mini);
   printf("Av max: %i  Av max index: %i\n", av_max, av_maxi);
+
+  selectParents(p_index);
+}
+
+void sort(int array[]){
+  for (int i = 9; i > 0; --i) {
+    // index
+    int w = rand() % i;
+    // swap
+    int t = array[i];
+    array[i] = array[w];
+    array[w] = t;
+  }
+}
+
+int getAv(int array[]) {
+  int aux;
+  aux = (array[0] * 1000 + array[1] * 100 + array[2] * 10 + array[3]) +
+        (array[4] * 1000 + array[5] * 100 + array[6] * 10 + array[1]) -
+        (array[4] * 10000 + array[5] * 1000 + array[2] * 100 + array[1] * 10 +
+         array[7]);
+  return abs(aux);
+}
+
+void selectParents(int p_index[]){
+  printf("INDEXS \n");
+  for(int n =0; n <80; n++){
+    p_index[n] = rand()%80;
+    printf("%i \n", p_index[n]);
+  }
 }
