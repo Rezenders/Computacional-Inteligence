954cd35877194ae3ecf32f8273ab2c7a78444cde
Gustavo Rezende Silva <gustavorezendesilva@hotmail.com>
2017-04-18 20:00:58 -0300

    Fazendo crossover aplicando mutacao e com isso gerando filhos. Falta atualizar a pop

--- AG/ag.c
+++ AG/ag.c
@@ -3,47 +3,61 @@
 #include "time.h"
 #include <string.h>
 
-void sort(int array[]);
-int getAv(int array[]);
+void generatePop(int pop[][11], int pop_size);
+void sortRandom(int array[]);
+void getAv(int array[]);
 void getMinMaxAv();
-void tour(int parents[][11], int p_index[], int tour_size);
-void crossOver(int p_index[]);
+void tour(int parents[][11], int p_index[], int tour_size, int n_sons);
+void crossOverAll(int parents[][11], int p_index[], int sons[][11], int n_sons,
+                  int mutate_percent);
+void crossOver(int parent1[], int parent2[], int son1[], int son2[],
+               int mutate_percent);
+void mutation(int array[]);
+
+int av_min = 1000000000, av_max = 0;
+int av_mini = 0, av_maxi = 0;
 
 int main() {
   // sendmory
-  int p[100][11];
-  int p_index[80];
+  int pop_size = 100;
+  int n_sons = 80;
+  int mutate_percent = 10;
 
-  int n[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-  int av_min = 1000000000, av_max = 0;
-  int av_mini = 0, av_maxi = 0;
+  int p[pop_size][11];
+  int p_index[80];
+  int sons[n_sons][11];
 
   srand((unsigned)666);
 
-  for (int j = 0; j < 100; j++) {
-    sort(n);
-    memcpy(p[j], n, sizeof(int) * 10);
+  generatePop(p, pop_size);
+  tour(p, p_index, 3, n_sons);
+  crossOverAll(p, p_index, sons, n_sons, mutate_percent);
 
-    p[j][10] = getAv(p[j]);
+}
 
-    if (p[j][10] < av_min) {
-      av_min = p[j][10];
+void generatePop(int pop[][11], int pop_size) {
+  int n[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+  for (int j = 0; j < pop_size; j++) {
+    sortRandom(n);
+    memcpy(pop[j], n, sizeof(int) * 10);
+
+    getAv(pop[j]);
+
+    if (pop[j][10] < av_min) {
+      av_min = pop[j][10];
       av_mini = j;
     }
 
-    if (p[j][10] > av_max) {
-      av_max = p[j][10];
+    if (pop[j][10] > av_max) {
+      av_max = pop[j][10];
       av_maxi = j;
     }
   }
-  // printf("size of int %i \n", sizeof(int));
   printf("Av min: %i  Av min index: %i\n", av_min, av_mini);
   printf("Av max: %i  Av max index: %i\n", av_max, av_maxi);
-
-  tour(p, p_index, 3);
 }
 
-void sort(int array[]) {
+void sortRandom(int array[]) {
   for (int i = 9; i > 0; --i) {
     // index
     int w = rand() % i;
@@ -54,19 +68,18 @@ void sort(int array[]) {
   }
 }
 
-int getAv(int array[]) {
+void getAv(int array[]) {
   int aux;
   aux = (array[0] * 1000 + array[1] * 100 + array[2] * 10 + array[3]) +
         (array[4] * 1000 + array[5] * 100 + array[6] * 10 + array[1]) -
         (array[4] * 10000 + array[5] * 1000 + array[2] * 100 + array[1] * 10 +
          array[7]);
-  return abs(aux);
+  array[10] = abs(aux);
 }
 
-void tour(int parents[][11], int p_index[], int tour_size) {
-  printf("INDEXS \n");
+void tour(int parents[][11], int p_index[], int tour_size, int n_sons) {
 
-  for (int i = 0; i < 80; i++) {
+  for (int i = 0; i < n_sons; i++) {
     int best_av = 0;
     int best_index = -1;
 
@@ -81,7 +94,53 @@ void tour(int parents[][11], int p_index[], int tour_size) {
 
       p_index[i] = best_index;
     }
-
-    printf("%i \n", p_index[i]);
   }
+}
+
+void crossOverAll(int parents[][11], int p_index[], int sons[][11], int n_sons,
+                  int mutate_percent) {
+  for (size_t i = 0; i < n_sons; i += 2) {
+    crossOver(parents[i], parents[i + 1], sons[i], sons[i + 1], mutate_percent);
+  }
+}
+
+void crossOver(int parent1[], int parent2[], int son1[], int son2[],
+               int mutate_percent) {
+  unsigned int index = rand() % 10;
+  unsigned int cycle[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+  while (!cycle[index]) {
+    cycle[index] = 1;
+    for (size_t i = 0; i < 10; i++) {
+      if (parent2[index] == parent1[i]) {
+        index = i;
+        break;
+      }
+    }
+  }
+
+  for (size_t j = 0; j < 10; j++) {
+    if (cycle[j] == 1) {
+      son1[j] = parent1[j];
+      son2[j] = parent2[j];
+    } else {
+      son1[j] = parent2[j];
+      son2[j] = parent1[j];
+    }
+  }
+  if ((rand() % 101) < mutate_percent) {
+    mutation(son1);
+  }
+  if ((rand() % 101) < mutate_percent) {
+    mutation(son2);
+  }
+
+  getAv(son1);
+  getAv(son2);
+}
+
+void mutation(int array[]) {
+  int index = rand() % 8;
+  int temp = array[index];
+  array[index] = array[8];
+  array[8] = array[index];
 }
