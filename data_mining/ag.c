6fe15f4db098625f817dfe1099cc3ec1286d854d
Gustavo Rezende Silva <gustavorezendesilva@hotmail.com>
2017-06-14 14:27:15 -0300

    ✨ Gerando regras aleatoriamente

--- data_mining/ag.c
+++ data_mining/ag.c
@@ -1,9 +1,10 @@
 #include "stdio.h"
+#include "stdlib.h"
 
 struct regra{
   double pesos[34];
-  char atributos[34];
-  char operadores[34];
+  int atributos[34];
+  int operadores[34]; // =, !=, <, >=
 };
 
 void geraPop(struct regra r[], int n_regras);
@@ -15,13 +16,23 @@ void atualizaPop();
 int main(){
   FILE *fp = fopen("data_base", "r");
 
+  srand(666);
+
   int n_regras = 30;
   struct regra r[n_regras];
   geraPop(r, n_regras);
 }
 
 void geraPop(struct regra r[], int n_regras){
-  
+  for(int i=0; i < n_regras; i++){
+    for(int j=0; j<34; j++){
+        r[i].pesos[j] = rand()%1001 / 1000;
+        r[i].atributos[j] = rand()%4;
+        r[i].operadores[j] = rand()%4;
+    }
+    r[i].atributos[10] = rand()%2;
+    r[i].atributos[33] = rand()%2;
+  }
   printf("Regras geradas aleatoriamente\n");
 }
 
