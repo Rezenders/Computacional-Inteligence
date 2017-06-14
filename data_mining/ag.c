22b32332a759df0ffd1ff2ad2c215e2b39aa5a11
Gustavo Rezende Silva <gustavorezendesilva@hotmail.com>
2017-06-14 14:57:26 -0300

    ✨ lendo do banco dermatology

--- data_mining/ag.c
+++ data_mining/ag.c
@@ -7,20 +7,27 @@ struct regra{
   int operadores[34]; // =, !=, <, >=
 };
 
+void readFile(int p_tre[][34], int tre_size, int p_tst[][34], int tst_size);
+
 void geraPop(struct regra r[], int n_regras);
 void geraAv(struct regra r[], int n_regras, char pacientes[][34], int n_pacientes );
 void roletaEst();
 void crossOver();
 void atualizaPop();
+void printRegras(struct regra r[], int n_regras);
 
 int main(){
-  FILE *fp = fopen("data_base", "r");
+  int p_tre[239][34];
+  int p_tst[119][34];
+  readFile(p_tre,239,p_tst,119);
 
   srand(666);
 
   int n_regras = 30;
   struct regra r[n_regras];
   geraPop(r, n_regras);
+
+  // printRegras(r, n_regras);
 }
 
 void geraPop(struct regra r[], int n_regras){
@@ -31,11 +38,35 @@ void geraPop(struct regra r[], int n_regras){
         r[i].operadores[j] = rand()%4;
     }
     r[i].atributos[10] = rand()%2;
-    r[i].atributos[33] = rand()%2;
+    r[i].atributos[33] = rand()%71;
   }
-  printf("Regras geradas aleatoriamente\n");
 }
 
 void geraAv(struct regra r[], int n_regras, char pacientes[][34], int n_pacientes ){
   printf("Avaliando o conjunto de regras\n");
+}
+
+void printRegras(struct regra r[], int n_regras){
+  for (size_t i = 0; i < n_regras; i++) {
+    for (size_t j = 0; j < 34; j++) {
+      printf("%d ",r[i].atributos[j]);
+    }
+    printf("\n");
+  }
+}
+
+void readFile(int p_tre[][34], int tre_size, int p_tst[][34], int tst_size){
+  FILE *fp = fopen("data_base", "r");
+
+  for (size_t j = 0; j < tre_size; j++) {
+    for (size_t j = 0; j < 34; j++) {
+      fscanf(fp,"%d,",&p_tre[j][j]);
+    }
+  }
+  for (size_t j = 0; j < tst_size; j++) {
+    for (size_t j = 0; j < 34; j++) {
+      fscanf(fp,"%d,",&p_tst[j][j]);
+    }
+  }
+  fclose(fp);
 }
