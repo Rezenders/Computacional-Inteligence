62219c5fa52e04f527606e6aba7e9e0a5cbd0050
Gustavo Rezende Silva <gustavorezendesilva@hotmail.com>
2017-06-15 18:05:06 -0300

    ✨ printando todas as regras encontradas

--- data_mining/ag.c
+++ data_mining/ag.c
@@ -3,94 +3,100 @@
 #include <stdbool.h>
 #include <string.h>
 
-struct regra{
-  double pesos[34];
-  int atributos[34];
-  int operadores[34]; // =, !=, <, >=
+struct rule{
+  double weights[34];
+  int att[34];
+  int operators[34]; // =, !=, <, >=
   double sp, se;
   double av;
 };
 
 void readFile(int p_tre[][35], int tre_size, int p_tst[][35], int tst_size);
-void quickSort(struct regra r[], int low, int high);
-int partition (struct regra r[], int low, int high);
-void swap(struct regra *a, struct regra *b);
+void quickSort(struct rule r[], int low, int high);
+int partition (struct rule r[], int low, int high);
+void swap(struct rule *a, struct rule *b);
 
-void geraPop(struct regra r[], int n_regras);
-void geraAv(struct regra r[], int n_regras, int pacientes[][35], int n_pacientes, int classe);
-void tourEst(struct regra r[], int n_regras,  int p_index[], int n_sons, int tour_size);
-void crossOver(struct regra r[], struct regra r_sons[], int p_index[], int n_sons, double mutate_percent);
-void updatePop(struct regra r[], struct regra r_sons[], int n_regras, int n_sons);
-void printRegras(struct regra r[], int n_regras);
+void genPop(struct rule r[], int n_rules);
+void getAv(struct rule r[], int n_rules, int patients[][35], int n_patients, int class);
+void tourEst(struct rule r[], int n_rules,  int p_index[], int n_sons, int tour_size);
+void crossOver(struct rule r[], struct rule r_sons[], int p_index[], int n_sons, double mutate_percent);
+void updatePop(struct rule r[], struct rule r_sons[], int n_rules, int n_sons);
+void printRule(struct rule r[], int n_rules, int class);
+void printRulesMembers(struct rule r[], int n_rules);
+void printBestRuleAv(struct rule r[], int n_rules);
+
+double max_weight = 0.3;
 
 int main(){
   //PARAMETROS DO AG
-  int n_regras = 50;
+  int n_rules = 50;
   double cross_over = 1;
-  int n_sons = cross_over*n_regras;
+  int n_sons = cross_over*n_rules;
   double mutate_percent = 0.3;
   int n_ger = 50;
 
-  //Pacientes de treinamento e teste
+  //patients de treinamento e teste
   int p_tre[239][35];
   int p_tst[119][35];
   readFile(p_tre,239,p_tst,119);
 
   //Pais e Filhos
-  struct regra r[n_regras];
-  struct regra r_sons[n_sons];
+  struct rule r[n_rules];
+  struct rule r_sons[n_sons];
   int p_index[n_sons];
 
   srand(999);
 
-  for(int classe=1; classe<=6; classe++){
-    geraPop(r, n_regras);
-    geraAv(r, n_regras, p_tre, 239, classe);
+  for(int class=1; class<=6; class++){
+    genPop(r, n_rules);
+    getAv(r, n_rules, p_tre, 239, class);
 
     for (size_t geracao = 0; geracao <n_ger; geracao++) {
-      tourEst(r, n_regras, p_index, n_sons, 3);
+      tourEst(r, n_rules, p_index, n_sons, 3);
       crossOver(r, r_sons, p_index, n_sons, mutate_percent);
-      geraAv(r_sons, n_sons, p_tre, 239, classe);
-      updatePop(r, r_sons, n_regras, n_sons);
+      getAv(r_sons, n_sons, p_tre, 239, class);
+      updatePop(r, r_sons, n_rules, n_sons);
     }
-    printf("Classe numero %d \n",classe);
-    printRegras(r, n_regras);
+    // printf("class numero %d \n",class);
+    // printBestRuleAv(r, n_rules);
+    printRule(r, n_rules, class);
+
   }
 }
 
-void geraPop(struct regra r[], int n_regras){
-  for(int i=0; i < n_regras; i++){
+void genPop(struct rule r[], int n_rules){
+  for(int i=0; i < n_rules; i++){
     for(int j=0; j<34; j++){
-        r[i].pesos[j] =(rand()%1001)/1000.0;
-        r[i].atributos[j] = rand()%4;
-        r[i].operadores[j] = rand()%4;
+        r[i].weights[j] =(rand()%1001)/1000.0;
+        r[i].att[j] = rand()%4;
+        r[i].operators[j] = rand()%4;
     }
-    r[i].atributos[10] = rand()%2;
-    r[i].atributos[33] = rand()%71;
+    r[i].att[10] = rand()%2;
+    r[i].att[33] = rand()%71;
   }
 }
 
-void geraAv(struct regra r[], int n_regras, int pacientes[][35], int n_pacientes, int classe){
-  double max_peso = 0.3;
-  for (size_t i = 0; i < n_regras; i++) {
+void getAv(struct rule r[], int n_rules, int patients[][35], int n_patients, int class){
+
+  for (size_t i = 0; i < n_rules; i++) {
     int tp=0, tn=0, fp=0, fn=0;
-    for (size_t j = 0; j < n_pacientes; j++) {
+    for (size_t j = 0; j < n_patients; j++) {
       bool is_class = true;
 
       for (size_t w = 0; w < 34; w++) {
-        if(r[i].pesos[w]<max_peso){
-          switch (r[i].operadores[w]) {
+        if(r[i].weights[w]<max_weight){
+          switch (r[i].operators[w]) {
             case 0:
-              is_class &= (pacientes[j][w] == r[i].atributos[w]);
+              is_class &= (patients[j][w] == r[i].att[w]);
             break;
             case 1:
-              is_class &= (pacientes[j][w] != r[i].atributos[w]);
+              is_class &= (patients[j][w] != r[i].att[w]);
             break;
             case 2:
-              is_class &= (pacientes[j][w] < r[i].atributos[w]);
+              is_class &= (patients[j][w] < r[i].att[w]);
             break;
             case 3:
-              is_class &= (pacientes[j][w] >= r[i].atributos[w]);
+              is_class &= (patients[j][w] >= r[i].att[w]);
             break;
           }
 
@@ -98,13 +104,13 @@ void geraAv(struct regra r[], int n_regras, int pacientes[][35], int n_pacientes
         }
       }
 
-      if(pacientes[j][34]==classe && is_class){
+      if(patients[j][34]==class && is_class){
         tp++;
-      }else if(pacientes[j][34]!=classe && is_class){
+      }else if(patients[j][34]!=class && is_class){
         fp++;
-      }else if(pacientes[j][34]!=classe && !is_class){
+      }else if(patients[j][34]!=class && !is_class){
         tn++;
-      }else if((pacientes[j][34]==classe) && !is_class){
+      }else if((patients[j][34]==class) && !is_class){
         fn++;
       }
     }
@@ -118,15 +124,15 @@ void geraAv(struct regra r[], int n_regras, int pacientes[][35], int n_pacientes
   // printf("Avaliando o conjunto de regras\n");
 }
 
-void tourEst(struct regra r[], int n_regras,  int p_index[], int n_sons, int tour_size){
+void tourEst(struct rule r[], int n_rules,  int p_index[], int n_sons, int tour_size){
   for (size_t i = 0; i < n_sons; i++) {
     int index_tour[tour_size];
     double roulette[tour_size];
     double av_total = 0;
 
     for (size_t j = 0; j < tour_size; j++) {
-      index_tour[j] = (rand()%n_regras);
-      av_total += r[(rand()%n_regras)].av;
+      index_tour[j] = (rand()%n_rules);
+      av_total += r[(rand()%n_rules)].av;
     }
 
     if(av_total!=0){
@@ -159,7 +165,7 @@ void tourEst(struct regra r[], int n_regras,  int p_index[], int n_sons, int tou
   // }
 }
 
-void crossOver(struct regra r[], struct regra r_sons[], int p_index[], int n_sons, double mutate_percent){
+void crossOver(struct rule r[], struct rule r_sons[], int p_index[], int n_sons, double mutate_percent){
   int n_points =2;
   int points[n_points];
   for (size_t i = 0; i < n_sons-1; i+=2) {
@@ -177,88 +183,117 @@ void crossOver(struct regra r[], struct regra r_sons[], int p_index[], int n_son
     r_sons[i] = r[i];
     r_sons[i+1] = r[i+1];
     for (size_t m = 0; m < n_points; m++) {
-      struct regra aux;
+      struct rule aux;
       aux = r_sons[i];
-      memcpy(r_sons[i].pesos, r_sons[i+1].pesos, sizeof(double) *points[m]);
-      memcpy(r_sons[i].atributos, r_sons[i+1].atributos, sizeof(int) *points[m]);
-      memcpy(r_sons[i].operadores, r_sons[i+1].operadores, sizeof(int) *points[m]);
+      memcpy(r_sons[i].weights, r_sons[i+1].weights, sizeof(double) *points[m]);
+      memcpy(r_sons[i].att, r_sons[i+1].att, sizeof(int) *points[m]);
+      memcpy(r_sons[i].operators, r_sons[i+1].operators, sizeof(int) *points[m]);
 
-      memcpy(r_sons[i+1].pesos, aux.pesos, sizeof(double) *points[m]);
-      memcpy(r_sons[i+1].atributos, aux.atributos, sizeof(int) *points[m]);
-      memcpy(r_sons[i+1].operadores, aux.operadores, sizeof(int) *points[m]);
+      memcpy(r_sons[i+1].weights, aux.weights, sizeof(double) *points[m]);
+      memcpy(r_sons[i+1].att, aux.att, sizeof(int) *points[m]);
+      memcpy(r_sons[i+1].operators, aux.operators, sizeof(int) *points[m]);
 
     }
-    if((rand()%1001/1000.0)<=0.3){
-      r_sons[i].pesos[(rand()%34)] = (rand()%1001/1000.0);
+    if((rand()%1001/1000.0)<=mutate_percent){
+      r_sons[i].weights[(rand()%34)] = (rand()%1001/1000.0);
     }
-    if((rand()%1001/1000.0)<=0.3){
+    if((rand()%1001/1000.0)<=mutate_percent){
       int index = (rand()%34);
       if(index!=10 && 33){
-        r_sons[i].atributos[index] = (rand()%4);
+        r_sons[i].att[index] = (rand()%4);
       }else if(index == 10){
-        r_sons[i].atributos[index] = (rand()%2);
+        r_sons[i].att[index] = (rand()%2);
       }else if(index == 33){
-        r_sons[i].atributos[index] = (rand()%71);
+        r_sons[i].att[index] = (rand()%71);
       }
     }
-    if((rand()%1001/1000.0)<=0.3){
-      r_sons[i].operadores[(rand()%34)] = (rand()%4);
+    if((rand()%1001/1000.0)<=mutate_percent){
+      r_sons[i].operators[(rand()%34)] = (rand()%4);
     }
-    if((rand()%1001/1000.0)<=0.3){
-      r_sons[i+1].pesos[(rand()%34)] = (rand()%1001/1000.0);
+    if((rand()%1001/1000.0)<=mutate_percent){
+      r_sons[i+1].weights[(rand()%34)] = (rand()%1001/1000.0);
     }
-    if((rand()%1001/1000.0)<=0.3){
+    if((rand()%1001/1000.0)<=mutate_percent){
       int index = (rand()%34);
       if(index!=10 && 33){
-        r_sons[i+1].atributos[index] = (rand()%4);
+        r_sons[i+1].att[index] = (rand()%4);
       }else if(index == 10){
-        r_sons[i+1].atributos[index] = (rand()%2);
+        r_sons[i+1].att[index] = (rand()%2);
       }else if(index == 33){
-        r_sons[i+1].atributos[index] = (rand()%71);
+        r_sons[i+1].att[index] = (rand()%71);
       }
     }
-    if((rand()%1001/1000.0)<=0.3){
-      r_sons[i+1].operadores[(rand()%34)] = (rand()%4);
+    if((rand()%1001/1000.0)<=mutate_percent){
+      r_sons[i+1].operators[(rand()%34)] = (rand()%4);
     }
 
   }
   // printf("\npai1: ");
   // for (size_t i = 0; i < 34; i++) {
-  //   printf("%d ",r[0].atributos[i]);
+  //   printf("%d ",r[0].att[i]);
   // }
   // printf("\npai2: ");
   // for (size_t i = 0; i < 34; i++) {
-  //   printf("%d ",r[1].atributos[i]);
+  //   printf("%d ",r[1].att[i]);
   // }
   // printf("\nfil1: ");
   // for (size_t i = 0; i < 34; i++) {
-  //   printf("%d ",r_sons[0].atributos[i]);
+  //   printf("%d ",r_sons[0].att[i]);
   // }
   // printf("\nfil2: ");
   // for (size_t i = 0; i < 34; i++) {
-  //   printf("%d ",r_sons[1].atributos[i]);
+  //   printf("%d ",r_sons[1].att[i]);
   // }
   // printf("\n");
 }
 
-void updatePop(struct regra r[], struct regra r_sons[], int n_regras, int n_sons){
-  struct regra aux[n_regras + n_sons];
-  memcpy(aux,r,sizeof(struct regra)*n_regras);
-  memcpy(&aux[50],r_sons,sizeof(struct regra)*n_sons);
+void updatePop(struct rule r[], struct rule r_sons[], int n_rules, int n_sons){
+  struct rule aux[n_rules + n_sons];
+  memcpy(aux,r,sizeof(struct rule)*n_rules);
+  memcpy(&aux[50],r_sons,sizeof(struct rule)*n_sons);
 
-  quickSort(aux,0, n_regras+n_sons-1);
-  memcpy(r, aux, sizeof(struct regra)*n_regras);
+  quickSort(aux,0, n_rules+n_sons-1);
+  memcpy(r, aux, sizeof(struct rule)*n_rules);
 }
 
-void printRegras(struct regra r[], int n_regras){
-  for (size_t i = 0; i < n_regras; i++) {
+void printRule(struct rule r[], int n_rules, int class){
+  quickSort(r, 0, n_rules-1);
+  printf("The resulting rule for class %d is: \n",class);
+  for (size_t i = 0; i < 34; i++) {
+    if(r[0].weights[i]< max_weight){
+      printf("att: %zu ",i);
+      switch (r[0].operators[i]) {
+        case 0:
+        printf("== ");
+        break;
+        case 1:
+        printf("!= ");
+        break;
+        case 2:
+        printf("< ");
+        break;
+        case 3:
+        printf(">= ");
+        break;
+      }
+      printf("%d \n", r[0].att[i]);
+    }
+  }
+}
+void printRulesMembers(struct rule r[], int n_rules){
+  for (size_t i = 0; i < n_rules; i++) {
     // for (size_t j = 0; j < 34; j++) {
-      // printf("%d ",r[i].operadores[j]);
-      // printf("%f ",r[i].pesos[j]);
+      // printf("%d ",r[i].operators[j]);
+      // printf("%f ",r[i].weights[j]);
     // }
     printf("%f ",r[i].av);
     printf("\n");
   }
+}
+
+void printBestRuleAv(struct rule r[], int n_rules){
+  quickSort(r, 0, n_rules-1);
+  printf("Best Av: %f\n", r[0].av);
 }
 
 void readFile(int p_tre[][35], int tre_size, int p_tst[][35], int tst_size){
@@ -291,7 +326,7 @@ void readFile(int p_tre[][35], int tre_size, int p_tst[][35], int tst_size){
   fclose(fp);
 }
 
-void quickSort(struct regra r[], int low, int high){
+void quickSort(struct rule r[], int low, int high){
     if (low < high)
     {
         /* pi is partitioning index, arr[p] is now
@@ -305,7 +340,7 @@ void quickSort(struct regra r[], int low, int high){
     }
 }
 
-int partition (struct regra r[], int low, int high){
+int partition (struct rule r[], int low, int high){
   // int pivot = arr[high];    // pivot
   double pivot = r[high].av;    // pivot
 
@@ -326,8 +361,8 @@ int partition (struct regra r[], int low, int high){
   return (i + 1);
 }
 
-void swap(struct regra *a, struct regra *b){
-  struct regra t = *a;
+void swap(struct rule *a, struct rule *b){
+  struct rule t = *a;
   *a = *b;
   *b = t;
 }
